99a7efefdce255aaab7fe0f5a38e66993b751daa
Vladimir Lopatinski <vlopatin@c1r4p6.hive.fi>
2025-03-03 14:18:38 +0200

    second commit from vlad-2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vlopatin <vlopatin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:02:44 by hiennguy          #+#    #+#             */
-/*   Updated: 2025/03/03 13:57:57 by vlopatin         ###   ########.fr       */
+/*   Updated: 2025/03/03 14:18:21 by vlopatin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,5 @@
 int main()
 {
 	//first commit from branch vlad-2
+	//2 commit from branch vlad-2
 }
