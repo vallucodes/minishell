769975ce8e426e3aca2ae49322fee0804a2fa3aa
Hien Nguyen <hiennguy@c1r1p8.hive.fi>
2025-03-05 14:30:16 +0200

    add hien's commit test 1 to see how merge works

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:02:44 by hiennguy          #+#    #+#             */
-/*   Updated: 2025/03/03 12:26:22 by hiennguy         ###   ########.fr       */
+/*   Updated: 2025/03/05 14:23:57 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 int	main ()
 {
+	//hien-test-commit-1
 	return (0);
 }
