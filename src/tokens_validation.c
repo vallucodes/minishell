7f886262ca6972410af17952d23e5c9a3e02c2d1
Vladimir Lopatinski <vlopatin@c2r2p6.hive.fi>
2025-03-19 11:41:12 +0200

    token validation working and printing error correctly

--- src/tokens_validation.c
+++ src/tokens_validation.c
@@ -1,22 +1,21 @@
 #include "minishell.h"
 
-int	validate_pipes_loop(t_token *current)
+static int	validate_tokens_looping(t_token *current)
 {
 	while (current != NULL)
 	{
-		if (current->next != NULL)
+		if (current->type == PIPE || current->type == REDIRECT_IN ||
+			current->type == REDIRECT_OUT || current->type == REDIRECT_APPEND ||
+			current->type == HERE_STRING)
 		{
-			if (current->type == PIPE && current->next->type == PIPE)
-			{
-				print_error(TOKEN_ERROR, PIPE_ERROR);
-				return (FAIL);
-			}
-		}
-		else
-		{
-			if (current->type == PIPE)
+			if (current->next == NULL)
 			{
 				print_error(TOKEN_ERROR, NEWLINE_ERROR);
+				return (FAIL);
+			}
+			else if (current->next->type == PIPE)
+			{
+				print_error(TOKEN_ERROR, PIPE_ERROR);
 				return (FAIL);
 			}
 		}
@@ -25,7 +24,7 @@ int	validate_pipes_loop(t_token *current)
 	return (SUCCESS);
 }
 
-static int	validate_pipes(t_token *tokens)
+static int	validate_tokens(t_token *tokens)
 {
 	t_token *current;
 
@@ -35,40 +34,14 @@ static int	validate_pipes(t_token *tokens)
 		print_error(TOKEN_ERROR, PIPE_ERROR);
 		return (FAIL);
 	}
-	if (validate_pipes_loop(current) == FAIL)
+	if (validate_tokens_looping(current) == FAIL)
 		return (FAIL);
-	return (SUCCESS);
-}
-
-static int	validate_redirections(t_token *tokens)
-{
-	t_token *current;
-
-	current = tokens;
-	while (current != NULL)
-	{
-		if ((current->type == REDIRECT_IN ||
-			current->type == REDIRECT_OUT ||
-			current->type == REDIRECT_APPEND ||
-			current->type == HERE_STRING))
-		{
-			print_tokens(tokens);
-			if (current->next == NULL)
-				print_error(TOKEN_ERROR, NEWLINE_ERROR);
-			else if (current->next->type == PIPE)
-				print_error(TOKEN_ERROR, PIPE_ERROR);
-			return (FAIL);
-		}
-		current = current->next;
-	}
 	return (SUCCESS);
 }
 
 int tokens_validation(t_token *tokens)
 {
-	// printf("return %i\n", validate_pipes(tokens));
-	if (validate_pipes(tokens) == FAIL ||
-		validate_redirections(tokens) == FAIL)
+	if (validate_pipes(tokens) == FAIL)
 		return (FAIL);
 	return (SUCCESS);
 }
