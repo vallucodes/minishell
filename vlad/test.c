70f79ba4c58c93a5a55b133dbd481ae3d81a9fa5
Vladimir Lopatinski <vlopatin@c2r2p8.hive.fi>
2025-03-21 10:40:59 +0200

    quotes concatination working in test file

--- vlad/test.c
+++ vlad/test.c
@@ -7,6 +7,11 @@ static int	is_separator(char c)
 	return(c == '<' || c == '>' || c == '|' || c == '<' || c == '\'' || c == '\"' || ft_isspace(c));
 }
 
+static int	is_quote(char c)
+{
+	return (c == '\'' || c == '\"');
+}
+
 static int	is_operator(char c)
 {
 	return (c == '<' || c == '>' || c == '|' || c == '<');
@@ -19,6 +24,7 @@ char	*con(char *str)
 	int	in_double;
 	int	in_quotes;
 	char	*new;
+	char	*temp;
 
 	i = 0;
 	in_single = 0;
@@ -34,22 +40,34 @@ char	*con(char *str)
 		if (is_operator(str[i]) && (in_double || in_single))
 		{
 			char additive[6] = {'\"', '\"', str[i], '\"' ,'\"', '\0'};
+			temp = new;
 			new = ft_strjoin(new, additive);
+			free (temp);
+			temp = NULL;
 		}
 		else
 		{
 			char additive[2] = {str[i], '\0'};
+			temp = new;
 			new = ft_strjoin(new, additive);
+			free (temp);
+			temp = NULL;
 		}
 		i++;
 	}
+
+
+	in_single = 0;
+	in_double = 0;
 	i = 0;
-	printf("new: 	   %s\n", new);
 	char *new2 = ft_strdup("");
 	if (!is_separator(new[i]))
 	{
 		char additive[3] = {'\"', str[i], '\0'};
+		temp = new;
 		new = ft_strjoin(new, additive);
+		free (temp);
+		temp = NULL;
 		i++;
 	}
 	while (new[i])
@@ -62,29 +80,74 @@ char	*con(char *str)
 		if (is_separator(new[i]) && !is_separator(new[i + 1]) && !in_quotes && new[i + 1] != '\0')
 		{
 			char additive[3] = {new[i], '\"', '\0'};
+			temp = new2;
 			new2 = ft_strjoin(new2, additive);
+			free (temp);
+			temp = NULL;
 		}
 		else if (!is_separator(new[i]) && is_separator(new[i + 1]) && !in_quotes)
 		{
 			char additive[3] = {new[i], '\"', '\0'};
+			temp = new2;
 			new2 = ft_strjoin(new2, additive);
+			free (temp);
+			temp = NULL;
 		}
 		else
 		{
 			char additive[2] = {new[i], '\0'};
+			temp = new2;
 			new2 = ft_strjoin(new2, additive);
+			free (temp);
+			temp = NULL;
 		}
 		i++;
 	}
-	return (new2);
+	free (new);
+
+
+	in_single = 0;
+	in_double = 0;
+	i = 0;
+	char *new3 = ft_strdup("");
+	while (new2[i])
+	{
+		if (new2[i] == '\'' && !in_double)
+			in_single = !in_single;
+		else if (new2[i] == '"' && !in_single)
+			in_double = !in_double;
+		in_quotes = in_double || in_single;
+		if (is_quote(new2[i]) && is_quote(new2[i + 1]) && !in_quotes)
+		{
+			i++;
+			if (new2[i] == '\'' && !in_double)
+				in_single = !in_single;
+			else if (new2[i] == '"' && !in_single)
+				in_double = !in_double;
+			in_quotes = in_double || in_single;
+		}
+		else
+		{
+			char additive[2] = {new2[i], '\0'};
+			temp = new3;
+			new3 = ft_strjoin(new3, additive);
+			free (temp);
+			temp = NULL;
+		}
+		i++;
+	}
+	free (new2);
+	return (new3);
 }
 
 
 int main()
 {
 	// char str[] = "\"asd\"";
-	char str[] = "<asd\"as<dasd\"<123> | asdsad fd'sd'\"asd\"";
+	char str[] = "<asd\"as<dasd\"<123> | asdsad fd's\"\"\"\"d'\"asd\"";
 	printf("original:  %s\n", str);
-	printf("new2: 	   %s\n", con(str));
-	printf("should be: <\"asdas<dasd\"<\"123\"> | \"asdsad\" \"fdsdasd\"\n");
+	char *str2 = con(str);
+	printf("new3: 	   %s\n", con(str));
+	printf("should be: <\"asdas<dasd\"<\"123\"> | \"asdsad\" \"fds\"\"\"\"dasd\"\n");
+	free (str2);
 }
