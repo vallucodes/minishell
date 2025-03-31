9c65ce8f0e9785122d5709f0f47066afa2114797
Hien Nguyen <hiennguy@c3r5p3.hive.fi>
2025-03-31 20:15:56 +0300

    ft_env work and give right exit code

---
+++ src/builtins/execute_builtins.c
@@ -0,0 +1,41 @@
+#include "../../inc/minishell.h"
+#include "../../inc/builtin.h"
+
+
+
+// -------------------- MAIN TEST --------------------
+int	ft_strtestcmp( char *s1, const char *s2)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	i = 0;
+	while (str1[i] == str2[i] && str1[i])
+		i++;
+	return (str1[i] - str2[i]);
+}
+// -------------------- end TEST --------------------
+
+int execute_builtin(t_minishell *mshell, t_ast_test *ast)
+{
+	if (!ast || !ast->cmd || !ast->cmd[0])
+		return (FAIL);
+
+	printf("DEBUG: cmd[0] in exebuilt = %s\n", ast->cmd[0]);
+	printf("DEBUG: strcmp = %d\n", ft_strtestcmp(ast->cmd[0], "env"));
+
+
+	if (ft_strcmp(ast->cmd[0], "env") == 0)
+	{
+		mshell->exitcode = ft_env(mshell->envp, &ast->cmd[1]);
+		printf ("yo");
+		fprintf(stderr, "[builtin env exit code: %d]\n", mshell->exitcode);
+		return (mshell->exitcode);
+	}
+
+
+}
+
---
+++ src/builtins/ft_env.c
@@ -0,0 +1,24 @@
+#include "../../inc/minishell.h"
+
+int ft_env(t_env *my_env, char **args)
+{
+	unsigned int	i;
+
+	if (!my_env || !my_env->envp)
+		return (FAIL);
+
+	if (args && args[0])
+	{
+		dprintf(STDERR_FILENO, "env: too many arguments\n");
+		return (1);
+	}
+
+	i = 0;
+	while(i < my_env->len)
+	{
+		if (my_env->envp[i] && (ft_strchr(my_env->envp[i], '='))) // change!
+			printf("%s\n", my_env->envp[i]);
+		i++;
+	}
+	return (0);
+}
