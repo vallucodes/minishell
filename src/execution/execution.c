1deec508af8982a42ef6c344e06cbf5089dc2032
Hien Nguyen <hiennguy@c2r2p8.hive.fi>
2025-04-11 13:50:32 +0300

    done multiple pipes

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -1,272 +1,205 @@
 #include "../../inc/minishell.h"
 #include <sys/wait.h>
 
-/* DOCUMENTATION
-1. Start at the root (topmost PIPE node)
-2. Always run the command on .next_left. That can include redir or builtins
-3. After executing .next_left, we:
-		node = node->next_right;
-		that means we teleport to the next PIPE node
-4. Repeat until node == NULL - end of pipeline
-
-EXAMPLE!
-        PIPE 1
-       /    \
-   cat      PIPE 2
-           /    \
-      grep       wc
-
-1. node = PIPE 1
-   → node->next_left = cat
-   → Run `cat`, setup stdout to pipe1[1]
-   → Close pipe1[1] in parent
-   → Move to: node = node->next_right → PIPE 2
-
-2. node = PIPE 2
-   → node->next_left = grep
-   → Run `grep`, setup stdin = pipe1[0], stdout = pipe2[1]
-   → Close pipe1[0], pipe2[1] in parent
-   → Move to: node = node->next_right → wc
-
-3. node = wc (this is just the last command in .next_left of the final pipe)
-   → Run `wc`, setup stdin = pipe2[0], stdout = STDOUT_FILENO
-   → Close pipe2[0] in parent
-   → Done!
--------------------------------------------------------------------------------
-*/
-
-
-//check whether the command has a file redirection
-// static t_execution init_execution(t_ast *branch)
-// {
-// 	t_execution exec;
-
-// 	exec.cmd_args = NULL;
-// 	exec.temp_fd[FD_IN] = STDIN_FILENO;
-// 	exec.temp_fd[FD_OUT] = STDOUT_FILENO;
-
-// 	while (branch)
-// 	{
-// 		if (branch->type == REDIRECT_IN || branch->type == HERE_STRING)
-// 			exec.temp_fd[FD_IN] = safe_open_redirect(branch->file, O_RDONLY, 0);
-// 		else if (branch->type == REDIRECT_OUT)
-// 			exec.temp_fd[FD_OUT] = safe_open_redirect(branch->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-// 		else if (branch->type == REDIRECT_APPEND)
-// 			exec.temp_fd[FD_OUT] ft_d= safe_open_redirect(branch->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-// 		else if (branch->type == COMMAND)
-// 			exec.cmd_args = branch->cmd; // need to detect builtins here
-// 		branch = branch->next_left;
-// 	}
-// 	return exec;
-// }
-
-
 
 static int	is_builtin(const char *cmd)
 {
 	if (!cmd)
 		return (0);
 	return (ft_strcmp(cmd, "echo") == 0 ||
-	ft_strcmp(cmd, "cd") == 0 ||
-	ft_strcmp(cmd, "pwd") == 0 ||
-	ft_strcmp(cmd, "export") == 0 ||
-	ft_strcmp(cmd, "unset") == 0 ||
-	ft_strcmp(cmd, "env") == 0 ||
-	ft_strcmp(cmd, "exit") == 0
-	);
+		ft_strcmp(cmd, "cd") == 0 ||
+		ft_strcmp(cmd, "pwd") == 0 ||
+		ft_strcmp(cmd, "export") == 0 ||
+		ft_strcmp(cmd, "unset") == 0 ||
+		ft_strcmp(cmd, "env") == 0 ||
+		ft_strcmp(cmd, "exit") == 0);
 }
 
-//was here yesterday, now we have the
-// =    at 0x49AF3EA: wait4 (wait4.c:30)
-// ==144325==    by 0x406294: execute_ast (execution.c:240)
-// ==144325==    by 0x4013DC: main (main.c:43)
+static void handle_redirections(t_ast *node, t_execution *exec)
+{
+	while (node)
+	{
+		if (node->type == REDIRECT_IN || node->type == HERE_DOCUMENT)
+			exec->temp_fd[FD_IN] = safe_open_redirect(node->file, O_RDONLY, 0);
+		else if (node->type == REDIRECT_OUT)
+			exec->temp_fd[FD_OUT] = safe_open_redirect(node->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		else if (node->type == REDIRECT_APPEND)
+			exec->temp_fd[FD_OUT] = safe_open_redirect(node->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		node = node->next_left;
+	}
+}
 
-//need allocate pids and wait for the last one.
-// int	wait_for_children(t_minishell *mshell)
-// {
-// 	int		i;
-// 	int		wstatus;
-// 	int		exit_code;
-// 	pid_t	pid;
+static t_ast *get_cmd_node(t_ast *node, char ***cmd_args)
+{
+	t_ast *cmd_node = NULL;
+	while (node)
+	{
+		if (node->type == COMMAND)
+		{
+			*cmd_args = node->cmd;
+			cmd_node = node;
+		}
+		node = node->next_left;
+	}
+	return (cmd_node);
+}
 
-// 	i = 0;
-// 	exit_code = 0;
-// 	while (i < mshell->command_count)
-// 	{
-// 		pid = waitpid(mshell->pids[i], &wstatus, 0);
+static int fork_redirection_only(t_minishell *mshell, t_execution *exec)
+{
+	pid_t pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		return (1);
+	}
 
-// 		if (pid == mshell->pids[mshell->command_count - 1])
-// 			exit_code = WEXITSTATUS(wstatus);
-// 		i++;
-// 	}
-// 	return (exit_code);
-// }
+	if (pid == 0)
+	{
+		if (exec->temp_fd[FD_IN] != STDIN_FILENO)
+			dup2(exec->temp_fd[FD_IN], STDIN_FILENO);
+		if (exec->temp_fd[FD_OUT] != STDOUT_FILENO)
+			dup2(exec->temp_fd[FD_OUT], STDOUT_FILENO);
+		close(exec->temp_fd[FD_IN]);
+		close(exec->temp_fd[FD_OUT]);
+		exit(0);
+	}
+	mshell->command_count++;
+	mshell->last_pid = pid;
+	if (exec->temp_fd[FD_IN] != STDIN_FILENO)
+		close(exec->temp_fd[FD_IN]);
+	if (exec->temp_fd[FD_OUT] != STDOUT_FILENO)
+		close(exec->temp_fd[FD_OUT]);
+	return (1);
+}
 
+//maybe can stat wutg this tmrr 11.4, add them to struct!
+static void setup_child_fds(int prev_fd, int *pipefd, int has_next_pipe, t_execution *exec)
+{
+	if (exec->temp_fd[FD_IN] != STDIN_FILENO)
+		dup2(exec->temp_fd[FD_IN], STDIN_FILENO);
+	else if (prev_fd != STDIN_FILENO)
+		dup2(prev_fd, STDIN_FILENO);
+
+	if (exec->temp_fd[FD_OUT] != STDOUT_FILENO)
+		dup2(exec->temp_fd[FD_OUT], STDOUT_FILENO);
+	else if (has_next_pipe)
+		dup2(pipefd[1], STDOUT_FILENO);
+
+	if (exec->temp_fd[FD_IN] != STDIN_FILENO)
+		close(exec->temp_fd[FD_IN]);
+	if (exec->temp_fd[FD_OUT] != STDOUT_FILENO)
+		close(exec->temp_fd[FD_OUT]);
+	if (prev_fd != STDIN_FILENO)
+		close(prev_fd);
+	if (has_next_pipe)
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
+}
 
 int	wait_for_children(t_minishell *mshell)
 {
-	int		i;
-	int		wstatus;
-	int		exit_code;
-
-	i = 0;
-	exit_code = 0;
-
+	int i = 0, wstatus, exit_code = 0;
 	while (i < mshell->command_count)
 	{
 		if (mshell->last_pid == wait(&wstatus))
 			exit_code = WEXITSTATUS(wstatus);
 		i++;
 	}
-	printf("Child process%d\n", mshell->command_count);
-	return (exit_code);
+	return exit_code;
 }
 
 void execute_ast(t_minishell *mshell, t_ast *ast)
 {
-	t_ast *node;
-	t_ast *cmd_node = NULL;
-	int pipefd[2];
-	pid_t	pid;
+	t_ast *cmd_node;
 	t_execution exec;
-	int has_next_pipe;
-	int prev_fd = STDIN_FILENO;
-	int status;
+	int pipefd[2], prev_fd = STDIN_FILENO, has_next_pipe;
+	pid_t pid;
 	char **external_cmd;
 
-	node = ast;
 	while (ast)
 	{
-		//You always run the command on the left side of the pipe node
-		//after running all the left one, then you move right
-		// exec = init_execution(node); //init for all redirection + command
-
-		node = ast; // ast should stay as it is!
-		//exec.cmd_args = NULL;
 		exec.temp_fd[FD_IN] = STDIN_FILENO;
 		exec.temp_fd[FD_OUT] = STDOUT_FILENO;
+		exec.cmd_args = NULL;
+		cmd_node = NULL;
+		has_next_pipe = (ast->next_right != NULL);
 
-		has_next_pipe = (ast->next_right != NULL); //PIPE ALWAYS ON RIGHT, if not null then there is next
 		if (has_next_pipe && pipe(pipefd) < 0)
-			{
-				perror("pipe");
-				return;
-			}
-
-		while (node)
 		{
-			if (node->type == REDIRECT_IN || node->type == HERE_DOCUMENT)
-				exec.temp_fd[FD_IN] = safe_open_redirect(node->file, O_RDONLY, 0);
-			else if (node->type == REDIRECT_OUT)
-				exec.temp_fd[FD_OUT] = safe_open_redirect(node->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			else if (node->type == REDIRECT_APPEND)
-				exec.temp_fd[FD_OUT] = safe_open_redirect(node->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			else if (node->type == COMMAND)
-			{
-				exec.cmd_args = node->cmd;
-				cmd_node = node;
-				printf("DEBUG: Found COMMAND node: %s\n", node->cmd[0]);
-			}
-			node = node->next_left;
+			perror("pipe");
+			return;
 		}
+		handle_redirections(ast, &exec);
+		cmd_node = get_cmd_node(ast, &exec.cmd_args);
 
-		//check if cmd args is null
 
-		if(!exec.cmd_args || !exec.cmd_args[0])
+
+		//if only redirection is here like  < Makefile >out23
+		if ((!exec.cmd_args || !exec.cmd_args[0]) &&
+			(exec.temp_fd[FD_IN] != STDIN_FILENO || exec.temp_fd[FD_OUT] != STDOUT_FILENO))
+		{
+			if (fork_redirection_only(mshell, &exec) < 0)
+				return;
+			ast = ast->next_right;
+			continue;
+		}
+		else if (!exec.cmd_args || !exec.cmd_args[0])
 		{
 			ast = ast->next_right;
 			continue;
 		}
 
-		// must handle builtin in parent if it's not in a pipe
-		//standalone builtin
 		if (is_builtin(exec.cmd_args[0]) && !has_next_pipe)
 		{
 			execute_builtin(mshell, exec.cmd_args);
 			ast = ast->next_right;
 			continue;
 		}
+
 		pid = fork();
-		if (pid == -1)
+		if (pid < 0)
 		{
 			perror("fork");
 			return;
 		}
-		if (pid == 0) // CHILD PROCESS STARTS HERE
+
+		if (pid == 0)
 		{
-			//INPUT SET UP - DUP
-
-			if (exec.temp_fd[FD_IN] != STDIN_FILENO)
-				dup2(exec.temp_fd[FD_IN], STDIN_FILENO); // redir stdin to infile
-			else if (prev_fd != STDIN_FILENO)
-				dup2(prev_fd, STDIN_FILENO); //redir current command's standard input
-											//(stdin) to come from the read end of the previous pipe.
-
-			//OUTPUT SETUP - DUPPING
-			if (exec.temp_fd[FD_OUT] != STDOUT_FILENO)
-				dup2(exec.temp_fd[FD_OUT], STDOUT_FILENO); //NEED CHECK DUP STATUS
-			else if (has_next_pipe)
-				dup2(pipefd[1], STDOUT_FILENO);
-
-			//CHECK UNUSED AND CLOSE!
-			if (exec.temp_fd[FD_IN] != STDIN_FILENO)
-				close(exec.temp_fd[FD_IN]); //NEED CHECK CLOSE STATUS
-			if (exec.temp_fd[FD_OUT] != STDOUT_FILENO)
-				close(exec.temp_fd[FD_OUT]);
-			if (prev_fd != STDIN_FILENO)
-				close(prev_fd);
-			if (has_next_pipe)
-			{
-				close(pipefd[0]);
-				close(pipefd[1]);
-			}
-			//printf("%s\n", get_token_type_name(node->type));
+			setup_child_fds(prev_fd, pipefd, has_next_pipe, &exec);
 			if (is_builtin(exec.cmd_args[0]))
 			{
 				execute_builtin(mshell, exec.cmd_args);
 				delete_minishell(mshell);
 				exit(mshell->exitcode);
 			}
-			if (cmd_node == NULL)
+			if (!cmd_node)
 			{
 				ft_dprintf(2, "minishell: command node not found\n");
 				exit(127);
 			}
-			// for (int i = 0; ast->cmd[i]; i++)
-			// {
-			// 	stdout = stderr;
-			// 	printf("[%i]: %s\n", i, ast->cmd[i]);
-			// }
-			external_cmd = get_command_argv(mshell, cmd_node); //cmd_node == current node as a command
+			external_cmd = get_command_argv(mshell, cmd_node);
 			execve(external_cmd[0], exec.cmd_args, mshell->envp->envp);
 			perror(external_cmd[0]);
 			ft_free_2d(external_cmd);
 			arena_destroy(&mshell->arena);
 			exit(127);
-			//if executable file exists but no permission --> 126
 		}
-		else //BACK TO PARENTS, after forking a child to handle a command in a pipeline
+		else
 		{
-
 			mshell->command_count++;
-			mshell->last_pid = pid; //storing current last pid
-			//prev_fd is the readend of the previous pipe, used as stdin of curr cmd
-			//parent no need this so we close!
+			mshell->last_pid = pid;
 			if (prev_fd != STDIN_FILENO)
 				close(prev_fd);
-
-			// exec.temp_fd[FD_IN] holds input redir <infile or heredoc, must close now
 			if (exec.temp_fd[FD_IN] != STDIN_FILENO)
 				close(exec.temp_fd[FD_IN]);
-
-			// exec.temp_fd[FD_OUT] holds output redir >outf >>file, must close now
 			if (exec.temp_fd[FD_OUT] != STDOUT_FILENO)
 				close(exec.temp_fd[FD_OUT]);
-
 			if (has_next_pipe)
 			{
-				close(pipefd[1]); //parents dont write shit
-				prev_fd = pipefd[0]; //pass read end to next cmd!
+				close(pipefd[1]);
+				prev_fd = pipefd[0];
 			}
 		}
 		ast = ast->next_right;
---
+++ src/execution/setup_child_fds.c
@@ -0,0 +1,40 @@
+#include "../../inc/minishell.h"
+#include <sys/wait.h>
+
+static void redirect_input(int prev_fd, t_execution *exec)
+{
+	if (exec->temp_fd[FD_IN] != STDIN_FILENO)
+		dup2(exec->temp_fd[FD_IN], STDIN_FILENO);
+	else if (prev_fd != STDIN_FILENO)
+		dup2(prev_fd, STDIN_FILENO);
+}
+
+static void redirect_output(int *pipefd, int has_next_pipe, t_execution *exec)
+{
+	if (exec->temp_fd[FD_OUT] != STDOUT_FILENO)
+		dup2(exec->temp_fd[FD_OUT], STDOUT_FILENO);
+	else if (has_next_pipe)
+		dup2(pipefd[1], STDOUT_FILENO);
+}
+
+static void close_unneeded_fds(int prev_fd, int *pipefd, int has_next_pipe, t_execution *exec)
+{
+	if (exec->temp_fd[FD_IN] != STDIN_FILENO)
+		close(exec->temp_fd[FD_IN]);
+	if (exec->temp_fd[FD_OUT] != STDOUT_FILENO)
+		close(exec->temp_fd[FD_OUT]);
+	if (prev_fd != STDIN_FILENO)
+		close(prev_fd);
+	if (has_next_pipe)
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
+}
+
+static void setup_child_fds(int prev_fd, int *pipefd, int has_next_pipe, t_execution *exec)
+{
+	redirect_input(prev_fd, exec);
+	redirect_output(pipefd, has_next_pipe, exec);
+	close_unneeded_fds(prev_fd, pipefd, has_next_pipe, exec);
+}
